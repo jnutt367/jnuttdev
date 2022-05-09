61d32c7211349c963d0097cc20fd76f2efa5c553
Jason Nutt <jnutt367@gmail.com>
2022-05-09 19:24:38 -0400

    commit for changed image for better continuity of branding

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
         />
         </a>
         <a href="https://www.linkedin.com/in/jnuttlovedisciple/"><Image
-          src="/jason-smiling.jpg"
+          src="/jason_waving.jpg"
           alt="Hero Image"
           height={200}
           width={200}
